d702026807c9d1b3fbbe364d82165379c7e6f322
kahitam <kahitam@gmail.com>
2023-12-10 23:41:55 +0700

    add superusers level

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,11 +11,25 @@ load_dotenv()
 
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 BOTNAME = os.getenv('BOTNAME')
+sUsers = os.getenv('SUPERUSERS')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+# Create a decorator that takes users as an argument and if the user is in the list, it will run the function
+def user_allowed(susers):
+    def decorator(func):
+        async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+            user = update.message.from_user
+            logging.info(f'User {user.username} is trying to access the bot. id: {user.id}, message: {update.message.text}')
+            if user.username in sUsers:
+                await func(update, context)
+            else:
+                await update.message.reply_text("Your are not allowed to use this command")
+        return wrapper
+    return decorator
 
 # Let us use the /start command
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -25,12 +39,17 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('This is help descriptions')
 
+@user_allowed(sUsers)
+async def channelinfo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('List of Channel info')
+
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
 
     # Commands
     app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('channelinfo', channelinfo_command))
     app.add_handler(CommandHandler('help', help_command))
 
     app.run_polling()
