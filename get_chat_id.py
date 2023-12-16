22cf867603329913af299c3755cd4d8b5aef9618
kahitam <kahitam@gmail.com>
2023-12-17 01:39:35 +0700

    remove unused file

--- get_chat_id.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv('TELEGRAM_TOKEN')
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-print(requests.get(url).json())
